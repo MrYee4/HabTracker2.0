82ef211b12f1c42ccc79a9f114febfa22f981cbb
JadynBug <bookgirldreams88@yahoo.com>
2021-07-01 11:31:34 -0600

    added two buttons to create task and shifted a few things in main task

--- app/src/main/java/com/example/habtracker20/CreateTask.java
+++ app/src/main/java/com/example/habtracker20/CreateTask.java
@@ -2,7 +2,9 @@ package com.example.habtracker20;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class CreateTask extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class CreateTask extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_task);
+
+        // Go to main activity
+        Button cancel = findViewById(R.id.cancelBtn);
+        cancel.setOnClickListener(vew -> {
+            Intent intent1 = new Intent(this, MainActivity.class);
+            startActivity(intent1);
+        });
     }
 }
