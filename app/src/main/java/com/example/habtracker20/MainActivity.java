9e699908e860657480a50b7497f2956872fd0bb0
Cindar <mon16015@byui.edu>
2021-07-06 19:23:27 -0600

    -Fixed Errors -Fixed Bugs -Fixed Gradle -Made Compile -Added Stable Calendar Bits -Started Implementation of Login (deprecated)

--- app/src/main/java/com/example/habtracker20/MainActivity.java
+++ app/src/main/java/com/example/habtracker20/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.habtracker20;
 
-import android.app.DatePickerDialog;
-import android.app.TimePickerDialog;
+//import android.app.DatePickerDialog;
+//import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 
+//import com.google.android.gms.auth.api.signin.GoogleSignIn;
+//import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+//import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+//import com.google.android.gms.common.SignInButton;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,10 +17,9 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.TimePicker;
-
-import java.util.Calendar;
+//import android.widget.DatePicker;
+//import android.widget.TimePicker;
+//import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +29,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        /* This was deprecated... researching further...
+        // Google Sign-In Creator
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        // Request API usage
+        public GoogleSignInOptions.Builder requestScopes (Scope R.string.apiClientID)
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        // Check for existing Google Sign In account
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(account);
+        // Set options of the sign-in button.
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.sign_in_button:
+                    signIn();
+                    break;
+                // ... }} */
 
         // Button to go to Calendar
         Button button = findViewById(R.id.button_first);
@@ -48,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent1);
         });
 
-/*
+        /*
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-*/
+        */
     }
 
     @Override
